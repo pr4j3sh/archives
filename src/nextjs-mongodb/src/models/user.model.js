beee95dd7f14a591a4da08913d976d4d0ed570d6
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-06-05 23:37:32 +0530

    added more user routes

--- src/nextjs-mongodb/src/models/user.model.js
+++ src/nextjs-mongodb/src/models/user.model.js
@@ -11,17 +11,14 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please provide an email"],
     unique: true,
   },
-
   password: {
     type: String,
     required: [true, "Please provide a password"],
   },
-
   isVerified: {
     type: Boolean,
     default: false,
   },
-
   isAdmin: {
     type: Boolean,
     default: false,
