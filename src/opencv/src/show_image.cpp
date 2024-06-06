575775a51a1c1eb0bb713ddc6d404dedc2d6a653
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-06-06 21:13:29 +0530

    added show image file with size and type

--- src/opencv/src/show_image.cpp
+++ src/opencv/src/show_image.cpp
@@ -1,10 +1,21 @@
 #include <opencv2/opencv.hpp>
+#include <typeinfo>
 
 using namespace cv;
 
 int main(int argc, char * argv[]){
+    // std::cout<<argc<<std::endl<<argv[1];
     Mat img = imread(argv[1],-1);
-    // std::cout<<img;
-    // std::cout<<Mat.size(); // -- wrong
+    std::cout<<"type: "<<typeid(img).name()<<std::endl;
+    std::cout<<"size [w x h]: "<<img.size()<<std::endl;
+    std::cout<<"type of size: "<<typeid(img.size()).name()<<std::endl;
+    if(img.empty()){
+        std::cout<<"No image provided"<<std::endl;
+        return -1;
+    }
+    namedWindow("Wallpaper - Rage", WINDOW_AUTOSIZE);
+    imshow("Wallpaper - Rage",img);
+    waitKey(0);
+    destroyWindow("Wallpaper - Rage");
     return 0;
 }
