ff0acf440541d76fecd967c4647b97ce85f15c31
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-06-07 18:47:27 +0530

    added scripts

--- src/nodejs/index.js
+++ src/nodejs/index.js
@@ -1,8 +1,8 @@
 const calc = require("./src/calc");
 const server = require("./src/web-server");
 
-const hostname = "127.0.0.1";
-const port = 5000;
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
 
 server.listen(port, hostname, () => {
   console.log(`server > http://${hostname}:${port}`);
