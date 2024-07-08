244600ed4eb74d5cba5de15b27e0b54a4ddacb0a
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-07-08 14:50:32 +0530

    added vitest

--- src/vite/main.js
+++ src/vite/main.js
@@ -1,10 +1,14 @@
 import "./style.css";
+import {add} from "./src/js/add"
 
 document.querySelector("#app").innerHTML = `
   <div class="container">
   <h1>Vite Vanilla</h1>
-  <p>Vanilla js build with vite</p>
+  <p>Vanilla javascript website build with <a href="https://vitejs.dev/">vite</a>.</p>
+  <h2>Sum</h2>
+  <p>Let's add two numbers and then test it using <a href="https://vitest.dev/">vitest</a>.</p>
   </div>
 `;
 
-setupCounter(document.querySelector("#counter"));
+const sum = add(2,3);
+console.log(sum);
