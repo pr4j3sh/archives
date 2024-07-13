21577c8d772c06dcaf645d7c730dac6a3913a2d7
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-07-14 01:17:54 +0530

    added basicauth logic

--- src/basicauth/src/js/auth/login.user.js
+++ src/basicauth/src/js/auth/login.user.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { config } from "../../../axios.config";
 const apiUrl = import.meta.env.VITE_API_URL + "/api/auth";
 
-export async function registerUser(e) {
+export async function loginUser(e) {
   e.preventDefault();
   try {
     const formData = {
@@ -12,7 +12,7 @@ export async function registerUser(e) {
     if (!formData.username || !formData.password) {
       alert("fill in all the fields");
     } else {
-      const res = await axios.post(apiUrl + "/register", formData, config);
+      const res = await axios.post(apiUrl + "/login", formData, config);
       console.log(res.data);
       localStorage.setItem("user", JSON.stringify(res.data.user));
       localStorage.setItem("status", JSON.stringify(true));
--- src/nodejs-server/src/controllers/auth.controllers.js
+++ src/nodejs-server/src/controllers/auth.controllers.js
@@ -1,10 +1,21 @@
 const fs = require("fs");
+const bcrypt = require("bcryptjs");
 const { database } = require("../../app.config");
 
 const registerUser = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = { username, password };
+    if (!username || !password) {
+      throw new Error("fill in all the fields");
+    }
+
+    const content = await fs.readFileSync(database);
+    const existingUser = JSON.parse(content);
+    if (existingUser.username === username) {
+      throw new Error("user already exists");
+    }
+    const hash = await bcrypt.hash(password, 10);
+    const user = { username, password: hash };
     await fs.writeFileSync(database, JSON.stringify(user));
     res.status(201).json({ message: "user registered", user });
   } catch (error) {
@@ -16,10 +27,22 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
   try {
     const { username, password } = req.body;
-    console.log({ username, password });
-    res.status(200).json({ message: "user logged in" });
+    if (!username || !password) {
+      throw new Error("fill in all the fields");
+    }
+    const content = await fs.readFileSync(database);
+    const user = JSON.parse(content);
+    if (user.username !== username) {
+      throw new Error("user not found");
+    }
+    const isAuthorised = await bcrypt.compare(password, user.password);
+    if(!isAuthorised){
+      throw new Error("invalid credentials")
+    }
+    res.status(200).json({ message: "user logged in", user });
   } catch (error) {
-    res.status(500).json({ message: error });
+    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
