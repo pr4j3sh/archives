d56e4b73062c953da2934f2c1c103cdff20f59a7
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-10-11 02:22:42 +0530

    added laravel notes get

---
+++ src/php/web/laravel/resources/views/note/index.blade.php
@@ -0,0 +1,25 @@
+<x-layout>
+    <div class="note-container">
+        <a href="{{ route('note.create') }}" class="new-note-btn">
+            New Note
+        </a>
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                        {{ Str::words($note->note, 20) }}
+                    </div>
+                    <div class="note-buttons">
+                        <a href="{{ route('note.show', $note) }}" class="note-edit-btn">
+                            View
+                        </a>
+                        <a href="{{ route('note.edit', $note) }}" class="note-edit-btn">
+                            Edit
+                        </a>
+                        <button class="note-delete-button">Delete</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</x-layout>
