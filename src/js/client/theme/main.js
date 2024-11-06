fc90c230062fc14d32bcf0527dca1288444ec6c1
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-11-06 17:48:05 +0530

    added theme

--- src/js/client/theme/main.js
+++ src/js/client/theme/main.js
@@ -1,1 +1,39 @@
 import "./style.css";
+
+const toggleButton = document.getElementById("theme-toggle");
+
+const currentTheme = localStorage.getItem("theme");
+
+const prefersDarkScheme = window.matchMedia(
+  "(prefers-color-scheme: dark)",
+).matches;
+
+if (currentTheme === "dark") {
+  document.documentElement.classList.add("dark");
+} else if (currentTheme === "light") {
+  document.documentElement.classList.remove("dark");
+} else if (prefersDarkScheme) {
+  document.documentElement.classList.add("dark");
+}
+
+toggleButton.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (e) => {
+    if (!localStorage.getItem("theme")) {
+      if (e.matches) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    }
+  });
