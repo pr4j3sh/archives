2a2fbe79a7da91b88094a14721844ecc550c066e
pr4j3sh <prajesh.eleven118@gmail.com>
2025-01-06 21:54:19 +0530

    added data-* attribute and stryles with js in cleint

--- src/js/server/container/index.js
+++ src/js/server/container/index.js
@@ -1,12 +1,37 @@
+const {
+  errorHandler,
+  notFoundHandler,
+  logHandler,
+  asyncHandler,
+  corsHandler,
+} = require("exhandlers");
 const express = require("express");
+const os = require("os");
 
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
+const origins = process.env.ORIGINS;
+
 const server = express();
 
-server.use("/", (req, res) => {
-  res.json({ message: "server is online" });
-});
+server.use(express.json());
+server.use(corsHandler(origins));
+server.use(logHandler());
+
+server.get(
+  "/api/check",
+  asyncHandler(async (req, res) => {
+    const hostname = os.hostname();
+    res.status(200).json({
+      success: true,
+      message: "server online",
+      hostname,
+    });
+  }),
+);
+
+server.use(notFoundHandler);
+server.use(errorHandler);
 
 server.listen(port, hostname, () => {
   console.log(`server running @ http://${hostname}:${port}`);
