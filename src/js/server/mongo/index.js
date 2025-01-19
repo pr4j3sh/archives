f109929ca2b6e958ff322bd4766ca79afc191241
pr4j3sh <prajesh.eleven118@gmail.com>
2025-01-19 22:31:40 +0530

    added mongo db

--- src/js/server/mongo/index.js
+++ src/js/server/mongo/index.js
@@ -4,6 +4,7 @@ const { exit } = require("process");
 async function initMongo(uri) {
   try {
     await mongoose.connect(uri);
+    console.log("connected to mongo db");
   } catch (error) {
     console.error(error);
     exit(1);
