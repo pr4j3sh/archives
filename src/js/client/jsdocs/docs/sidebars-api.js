7e2d7153348dcfa96e7a7cd6d46f568b6385c7d1
pr4j3sh <prajesh.eleven118@gmail.com>
2025-03-19 23:09:50 +0530

    added jsdoc docusaur

---
+++ src/js/client/jsdocs/docs/sidebars-api.js
@@ -0,0 +1,9 @@
+export default {
+  api: [
+    {
+      type: "category",
+      label: "Functions",
+      items: ["greet"],
+    },
+  ],
+};
---
+++ src/js/client/jsdocs/src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Returns a greeting message.
+ *
+ * @returns {string} The greeting message "Hello world!".
+ */
+function greet() {
+  return "Hello world!";
+}
